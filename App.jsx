163dcadc2ecc4886b4ceada8ec038fbe06a65526
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-05-26 19:35:41 -0300

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+import logoTrairas from './src/assets/trairas-logo.png'
 import React, { useEffect, useMemo, useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { supabase, LOCK_AT } from './supabase'

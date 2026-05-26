018010fa1acdb65143770a46d5a4072c195ffd52
vinibianchi85-droid <vinibianchi85@gmail.com>
2026-05-26 17:45:39 -0300

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://fgkmjirugsfmdnmquhby.supabase.co'
+const supabaseUrl = 'https://fgkmjirqgsfmdnmquhby.supabase.co'
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZna21qaXJxZ3NmbWRubXF1aGJ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk4MDU0NDIsImV4cCI6MjA5NTM4MTQ0Mn0.YpOwcRTKB2NbKcwl8xjXtCsSAJzArFh2RkjVfMh5eb4'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
